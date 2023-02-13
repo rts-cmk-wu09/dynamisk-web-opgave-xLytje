b467ddfd82782fe6af97e8f0ef8f4cb0c78e0582
xLytje <rasmuslytje@gmail.com>
2023-02-13 13:58:22 +0100

    added hero

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     // slå dig løs her... IORDEN
+
+    // Hero
+    let heroArray = [hero];
+
+    let hero_sections = document.querySelector(".hero");
+
+    heroArray.forEach((heros) => {
+        let billede = document.createElement("img");
+        let card = document.createElement("section");
+        let h1 = document.createElement("h1");
+        h1.setAttribute("id", "text");
+        let p = document.createElement("p");
+        let a = document.createElement("a");
+        let ikon = document.createElement("img");
+
+        billede.src = heros.image;
+        ikon.src = heros.icon;
+        h1.textContent = heros.headline;
+        p.textContent = heros.copy;
+        a.textContent = "Explore";
+
+        hero_sections.append(billede);
+        hero_sections.append(card);
+        card.append(h1);
+        card.append(p);
+        card.append(a);
+        a.append(ikon);
+        
+        a.setAttribute("href", "#");
+
+        let textColor = document.getElementById("text");
+        textColor.innerHTML = "Find out how we can <span class='orange'>save your time</span> in Backpacking in Europe"
+    });
+
+    // Services
     let services_section = document.querySelector(".services")
 
     services.forEach((service) => {
