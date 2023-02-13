ebdac38d1e89e66a97647108e6dce352411dc97f
xLytje <rasmuslytje@gmail.com>
2023-02-13 12:26:28 +0100

    starten af services (alt kan laves udfra det

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // slå dig løs her... 
+    // slå dig løs her... IORDEN
+    let services_section = document.querySelector(".services")
+
+    services.forEach((service) => {
+        let card = document.createElement("section");
+        card.setAttribute("class", "services_card");
+        let billede = document.createElement("img");
+        let title = document.createElement("h2");
+        let kort_tekst = document.createElement("p");
+        let kort_link = document.createElement("a");
+
+        billede.src = service.illustration;
+        title.textContent = service.headline;
+        kort_tekst.textContent = service.text;
+        kort_link.textContent = service.linktext;
+
+        card.append(billede);
+        card.append(title);
+        card.append(kort_tekst);
+        card.append(kort_link);
+        services_section.append(card);
+
+    })
 
     //eksempel på at udskrive alle overskrifter i services i konsollen:
     services.forEach(service => console.log(service.headline))
